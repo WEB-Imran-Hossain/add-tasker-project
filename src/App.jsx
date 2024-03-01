d76cdd43ed2d73065f7f139b7aeb7acf058b9cc9
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-03-01 10:17:08 +0600

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+
+import Footer from "./shared/Footer";
 import Header from "./shared/Header";
 
 export default function App (){
     return (
         <>
         <Header></Header>
+        <Footer></Footer>
         </>
     );
 }
