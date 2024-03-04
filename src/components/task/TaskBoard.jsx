29792c75ce64b4d938ae76f8d3b1d97a2142d2c4
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-03-04 20:17:25 +0600

    adding modal and input field activity

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -19,13 +19,16 @@ export default function TaskBoard() {
   const [tasks, setTask] = useState([defaultTask]);
   const [showAddModal, setShowAddModal] = useState(false);
 
-  function handleAddTask() {
-    console.log("Adding a Task");
+  function handleAddTask({tag}) {
+    console.log("Adding a Task...", tag);
   }
+const showModal= ()=>{
+  setShowAddModal(true)
+}
 
   return (
     <section className="mb-20" id="tasks">
-      {showAddModal && <AddTaskModal />}
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
       <div className="container">
         {/* Search Box */}
         <div className="p-2 flex justify-end">
@@ -33,7 +36,7 @@ export default function TaskBoard() {
         </div>
         {/* Search Box Ends */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions onAddClick={()=> setShowAddModal(true)} />
+          <TaskActions onAddClick={showModal} />
 
           <TaskList tasks={tasks} />
         </div>
