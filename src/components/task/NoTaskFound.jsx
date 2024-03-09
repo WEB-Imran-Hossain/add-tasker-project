7bb80d56f69a48dd72f9565a183fc8cfab5e580b
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-03-10 01:00:48 +0600

    search field working properly

--- src/components/task/NoTaskFound.jsx
+++ src/components/task/NoTaskFound.jsx
@@ -1,5 +1,7 @@
 export default function NoTaskFound(){
     return (
-        
+        <p className="text-center text-3xl">
+            No Task Found
+        </p>
     );
 }
--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -3,6 +3,7 @@ import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "../TaskList";
 import AddTaskModal from "./AddTaskModal";
+import NoTaskFound from "./NoTaskFound";
 
 export default function TaskBoard() {
   // table data create (as linke JSON file)
@@ -68,7 +69,16 @@ export default function TaskBoard() {
 
     newTasks[taskIndex].isFavourite = !newTasks[taskIndex].isFavourite;
 
-    setTasks(newTasks)
+    setTasks(newTasks);
+  }
+
+  function handleSearch(searchTerm) {
+    console.log(searchTerm);
+    const filtered = tasks.filter((task) =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setTasks([...filtered]);
   }
 
   return (
@@ -83,7 +93,7 @@ export default function TaskBoard() {
       <div className="container">
         {/* Search Box */}
         <div className="p-2 flex justify-end">
-          <SearchTask />
+          <SearchTask onSearch={handleSearch} />
         </div>
         {/* Search Box Ends */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
@@ -92,12 +102,16 @@ export default function TaskBoard() {
             onDeleteAllClick={handleDeleteAllClick}
           />
 
-          <TaskList
+        {
+          tasks.length > 0 ?
+          
+            (<TaskList
             tasks={tasks}
             onEdit={handleEditTask}
             onDelete={handleDeleteTask}
             onFav={handleFavorite}
-          />
+          />) : (<NoTaskFound />)
+        }
         </div>
       </div>
     </section>
